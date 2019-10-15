7ddd41b377abea4cc61e4978dfb4177ca5da11ca
QSD_k <info@qsdbih.com>
2019-10-15 15:31:08 +0200

    Use props destructuring

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,37 +1,21 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import * as userActions from '../redux/actions/userActions';
+import {filterUser} from '../redux/actions/userActions';
 
-class Search extends Component {
+const Search = ({filterUser}) => {
+    const onChange = e => filterUser(e.target.value);
 
-    onChange = (e) => {
-        this.props.actions.filterUser(e.target.value);
-    }
-
-    render() {
-        return (
-            <search className='searchLayout'>
-                <div className='searchText'><span>Find user</span></div>
-                <input
-                    type='text'
-                    className='userInput searchInput'
-                    placeholder='Search . . .'
-                    onChange={this.onChange}
-                    required/>
-            </search>
-        );
-    }
+    return (
+        <search className='searchLayout'>
+            <div className='searchText'><span>Find user</span></div>
+            <input
+                type='text'
+                className='userInput searchInput'
+                placeholder='Search . . .'
+                onChange={onChange}
+                required/>
+        </search>
+    );
 }
 
-function mapStateToProps(state, ownProps) { 
-    return{};
-}
-
-function mapDispatchToProps(dispatch) {
-    return{
-        actions: bindActionCreators(userActions, dispatch)
-    };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Search);
+export default connect(null, {filterUser})(Search);
