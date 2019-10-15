0d9fc4bf51dabbc3f6310244688da14c8920cb1d
QSD_k <info@qsdbih.com>
2019-10-15 15:45:02 +0200

    Refactor reducers

--- src/redux/reducers/userReducers.js
+++ src/redux/reducers/userReducers.js
@@ -13,34 +13,30 @@ const initialState = {
     }]
 }
 
-function addUser(state, action) {
-    return {
-        ...state,
-        user: [
-            ...state.user, action.user
-        ],
-        filteredUser: [
-            ...state.user, action.user
-        ]
-    };
+const addUser = (state, action) => ({
+    ...state,
+    user: [
+        ...state.user, action.user
+    ],
+    filteredUser: [
+        ...state.user, action.user
+    ]
+})
+
+const filterUsers = (state, action) => ({
+    ...state,
+    filteredUser: state.user.filter(user => user.name.toLowerCase().includes(action.user.toLowerCase()))
+})
+
+const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case types.ADD_USER:
+            return addUser(state, action);
+        case types.FILTER_USER:
+            return filterUsers(state, action);
+        default:
+            return state;
+    }
 }
 
-function filterUsers(state, action) {
-    console.log('state:',state)
-    return Object.assign({},  state, {
-            filteredUser: state.user.filter(user => 
-                user.name.toLowerCase().startsWith(action.user.toLowerCase()))    
-            });
-        }
-
-export default function userReducer (state = initialState, action) {
-        switch (action.type) {
-            case types.ADD_USER:
-                return addUser(state, action);
-            case types.FILTER_USER:
-                return filterUsers(state, action);
-        
-            default:
-                return state;
-        }
-}
+export default userReducer;
