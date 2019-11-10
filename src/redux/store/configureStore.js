aafae90034fda6ba6e5b16a8682c0c19684d6087
QSD_k <info@qsdbih.com>
2019-11-10 20:19:15 +0100

    Add redux thunk

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
 const configureStore = () => {
     return createStore(
         rootReducer,
-        applyMiddleware(reduxImmutableStateInvariant())
+        applyMiddleware(thunk, reduxImmutableStateInvariant())
     );
 };
 
